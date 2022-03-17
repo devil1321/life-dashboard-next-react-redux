2e5137803c2dce15f34def2fac2280abcd3103b5
Dominik Stepien <s.dominik1321@gmail.com>
2022-03-17 17:39:38 +0100

    fix errors

--- modules/api.module.ts
+++ modules/api.module.ts
@@ -10,14 +10,14 @@ export const fetchWheather = async () => {
             return res.data.city
         }
     })
-    .catch(err => console.log(err))
+    .catch((err:any) => console.log(err))
 
     const wheartherData = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
         params: {
             q: city,
             appid: 'd355aaa337c3ed0e0876c199a8060479'
         }
-    }).then((res:any) => res.data).catch(err => console.log(err))
+    }).then((res:any) => res.data).catch((err:any) => console.log(err))
 
     return wheartherData
 }
